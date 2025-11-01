4451b88585e486f47069f239b7955ae8006a80bf
humo-tech <yano.wni+git@gmail.com>
2025-11-01 14:05:27 +0000

    maps slug 追加

---
+++ app/components/Maps/Day01.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+    this is day01 map
+</template>
---
+++ app/pages/maps/[...slug].vue
@@ -0,0 +1,13 @@
+<script setup>
+const route = useRoute()
+const { data } = await useAsyncData(() => queryCollection('content').path(route.path).first())
+useSeoMeta({
+  title: data.value?.title,
+  description: data.value?.description
+})
+</script>
+
+<template>
+  <ContentRenderer v-if="data" :value="data" />
+  <div v-else>Page not found</div>
+</template>
