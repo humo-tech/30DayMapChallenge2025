3bafb4bb4d327c9828163198203e9a87f7768064
humo-tech <yano.wni+git@gmail.com>
2025-11-01 14:39:16 +0000

    set baseurl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-
+  app: {
+    baseURL: '/30DayMapChallenge2025/'
+  },
   modules: [
     '@nuxt/content',
     '@nuxt/eslint',
